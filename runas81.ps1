9d006d7c4f6bbf5297edbc77a31e3d329677465e
Evgeny Agafonchikov <evgeny.agafonchikov@akvelon.com>
2014-07-25 19:15:07 +0400

    first 'runas' version; works locally, there are issues on Azure

---
+++ runas81.ps1
@@ -0,0 +1,116 @@
+#TODO: uncomment for creating new user
+#NET USER AutoUser "SomePassword2014" /ADD
+#NET LOCALGROUP "Administrators" "AutoUser" /ADD
+
+# folder that is allowed to share between users
+mkdir "C:\mytemp"
+
+$user = "AutoUser"
+#TODO: store pw encripted
+$pass = "SomePassword2014" | convertto-securestring -asplaintext -force
+$cred = new-object -typename System.Management.Automation.PSCredential -argumentlist $user, $pass
+
+#generate Win8.1 provision script
+'echo $env:USERNAME
+$wc = New-Object System.Net.WebClient
+
+$slaveAddr = "localhost:9889"
+$buildbotDir = "C:/bb/"
+$slaveName = "slv_win"
+
+Write-Host "Installing Chocolatey ..."
+iex ((new-object net.webclient).DownloadString("https://chocolatey.org/install.ps1"))
+$env:Path+=";C:\ProgramData\chocolatey\bin"
+
+#Installing nodejs; Chocolatey cannott install it successfully under SYSTEM account
+# so we have to do it explicitly
+$source = "http://nodejs.org/dist/v0.10.29/node-v0.10.29-x86.msi"
+$destination = $($env:TEMP + "\node-v0.10.29-x86.msi")
+$wc.DownloadFile($source, $destination)
+
+msiexec /qn /norestart /l* $($env:TEMP + "\node_inst.log") /i $destination ALLUSERS=1
+$env:Path+=";C:\Program Files\nodejs\"
+
+#Git section
+choco install git.install
+Write-Host "Updating Git PATH ..."
+$oldPath=(Get-ItemProperty -Path "Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment" -Name PATH).Path
+$newPath=$oldPath+"C:\Program Files (x86)\Git\bin"
+#Next line will fail w/o Administrator privileges
+Set-ItemProperty -Path "Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment" -Name PATH –Value $newPath
+$env:Path+=";C:\Program Files (x86)\Git\cmd;C:\Program Files (x86)\Git\bin"
+
+#Python section
+choco install python2
+
+#Setup Tools section
+Write-Host "Installing pip ..."
+cinst pip
+$env:Path+=";C:\tools\python2\Scripts"
+
+Write-Host "Updating Python PATH ..."
+$oldPath=(Get-ItemProperty -Path "Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment" -Name PATH).Path
+$newPath=$oldPath+";C:\tools\python2;C:\tools\python2\Scripts"
+#Next line will fail w/o Administrator privileges
+Set-ItemProperty -Path "Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment" -Name PATH –Value $newPath
+[Environment]::SetEnvironmentVariable("Path", "$env:Path;C:\tools\python2;C:\tools\python2\Scripts", "Machine")
+
+cinst PyWin32
+
+#Buildbot prepare and install section
+Write-Host "Installing buildslave and dependencies ..."
+pip install buildbot-slave
+Write-Host ""
+
+#SDKs and VS install section
+
+choco install VisualStudioExpress2013Windows
+choco install VS2013.2
+
+choco install jdk8
+
+choco install ant
+
+choco install android-sdk
+Write-Host "Updating Android SDK PATH ..."
+$oldPath=(Get-ItemProperty -Path "Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment" -Name PATH).Path
+$newPath=$oldPath+";C:\Users\"+[Environment]::UserName+"\AppData\Local\Android\android-sdk"
+#Next line will fail w/o Administrator privileges
+Set-ItemProperty -Path "Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment" -Name PATH –Value $newPath
+
+android update sdk --no-ui 
+
+#Grunt-cli install section
+Write-Host "Installing Grunt-cli and dependencies ..."
+npm install -g grunt-cli
+$oldPath=(Get-ItemProperty -Path "Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment" -Name PATH).Path
+$newPath=$oldPath+";C:\Users\"+[Environment]::UserName+"\AppData\Roaming\npm"
+#Next line will fail w/o Administrator privileges
+Set-ItemProperty -Path "Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment" -Name PATH –Value $newPath
+Write-Host ""
+
+Show-WindowsDeveloperLicenseRegistration
+
+Install-WindowsFeature Desktop-Experience
+
+#buildslave start section
+mkdir $buildbotDir
+
+$argList = $("create-slave " + $buildbotDir + $slaveName + " " + $slaveAddr + " windows-slave pass")
+Start-Process -FilePath buildslave -ArgumentList $argList -Wait
+
+#TODO: Switch of buildslave start for now. Place it to autorun then
+#$argList = $("start " + $buildbotDir + $slaveName)
+#Start-Process -FilePath buildslave -ArgumentList $argList
+
+shutdown /r
+
+Write-Host "Press any key to continue ..."
+$x = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")' > "C:\mytemp\provision81.ps1"
+
+$arglist = "C:\mytemp\provision81.ps1"
+
+start-process powershell $arglist -Credential $cred
+
+Write-Host "Press any key to continue ..."
+$x = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
