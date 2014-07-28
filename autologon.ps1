27b3eba169b012825a57c48cf3563d2718f9bdb2
Evgeny Agafonchikov <evgeny.agafonchikov@akvelon.com>
2014-07-28 18:54:11 +0400

    adding autologon fixed version

---
+++ autologon.ps1
@@ -0,0 +1,7 @@
+# Autologon
+New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name AutoAdminLogon -Value 1
+New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name DefaultUserName -Value "AutoUser"
+New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name DefaultPassword -Value SomePassword2014
+$runValue = "powershell C:\mytemp\provision81.ps1"
+New-ItemProperty -Path registry::HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\RunOnce -Name Installer -Value $runValue
+shutdown /r
