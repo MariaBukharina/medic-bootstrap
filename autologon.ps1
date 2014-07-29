c74e0069f4be9fea1b12c0a4f5ba920aa2549b17
Evgeny Agafonchikov <evgeny.agafonchikov@akvelon.com>
2014-07-29 13:51:59 +0400

    removing intermediate script

--- autologon.ps1
+++ autologon.ps1
@@ -2,7 +2,7 @@
 New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name AutoAdminLogon -Value 1
 New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name DefaultUserName -Value "AutoUser"
 New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name DefaultPassword -Value SomePass2014
-$runValue = "powershell C:\mytemp\medic-bootstrap\creds81.ps1"
+$runValue = "powershell C:\mytemp\medic-bootstrap\win81.ps1"
 New-ItemProperty -Path registry::HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\RunOnce -Name Installer -Value $runValue
 
 shutdown /r
