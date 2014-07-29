29c6c85a17671ad43459468ae84f1f2e590d5f03
Evgeny Agafonchikov <evgeny.agafonchikov@akvelon.com>
2014-07-29 09:17:36 +0400

    updating autologon script

--- autologon.ps1
+++ autologon.ps1
@@ -2,6 +2,6 @@
 New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name AutoAdminLogon -Value 1
 New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name DefaultUserName -Value "AutoUser"
 New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name DefaultPassword -Value SomePassword2014
-$runValue = "powershell C:\mytemp\provision81.ps1"
+$runValue = "powershell C:\mytemp\medic-bootstrap\win81.ps1"
 New-ItemProperty -Path registry::HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\RunOnce -Name Installer -Value $runValue
 shutdown /r
