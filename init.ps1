3556047afcdbf01559e6cbb30ee35201dcf8dcd1
Evgeny Agafonchikov <evgeny.agafonchikov@akvelon.com>
2014-07-29 09:17:04 +0400

    updating init script

--- init.ps1
+++ init.ps1
@@ -5,8 +5,22 @@ NET LOCALGROUP "Administrators" "AutoUser" /ADD
 # folder that is allowed to share between users
 mkdir "C:\mytemp"
 
+Write-Host "Installing Chocolatey ..."
+iex ((new-object net.webclient).DownloadString("https://chocolatey.org/install.ps1"))
+$env:Path+=";C:\ProgramData\chocolatey\bin"
+
+choco install git.install
+Write-Host "Updating Git PATH ..."
+$oldPath=(Get-ItemProperty -Path "Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment" -Name PATH).Path
+$newPath=$oldPath+"C:\Program Files (x86)\Git\bin"
+#Next line will fail w/o Administrator privileges
+Set-ItemProperty -Path "Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment" -Name PATH –Value $newPath
+$env:Path+=";C:\Program Files (x86)\Git\cmd;C:\Program Files (x86)\Git\bin"
+
+git clone "https://github.com/akvelon/medic-bootstrap.git" "C:\mytemp\medic-bootstrap"
+
 Set-ItemProperty -Path registry::HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\policies\system -Name EnableLUA -Value 0
 
-New-ItemProperty -Path registry::HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\RunOnce -Name Installer -Value 'powershell "C:\mytemp\autologon.ps1"'
+New-ItemProperty -Path registry::HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\RunOnce -Name Installer -Value 'powershell "C:\mytemp\medic-bootstrap\autologon.ps1"'
 
 shutdown /r
