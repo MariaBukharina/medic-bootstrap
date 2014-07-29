14dc305e61abe9ff36707de628b0263c0af2ac57
Evgeny Agafonchikov <evgeny.agafonchikov@akvelon.com>
2014-07-29 09:47:11 +0400

    updating autologon with new password + delay for debugging

--- autologon.ps1
+++ autologon.ps1
@@ -1,7 +1,11 @@
 # Autologon
 New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name AutoAdminLogon -Value 1
 New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name DefaultUserName -Value "AutoUser"
-New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name DefaultPassword -Value SomePassword2014
-$runValue = "powershell C:\mytemp\medic-bootstrap\win81.ps1"
+New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name DefaultPassword -Value SomePass2014
+$runValue = "powershell C:\mytemp\medic-bootstrap\creds81.ps1"
 New-ItemProperty -Path registry::HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\RunOnce -Name Installer -Value $runValue
+
 shutdown /r
+
+Write-Host "Press any key to continue ..."
+$x = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
