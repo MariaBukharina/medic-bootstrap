7a9a19d3cc35a9cb9bf4b9e24a4c930294237d8c
Evgeny Agafonchikov <evgeny.agafonchikov@akvelon.com>
2014-07-30 09:41:57 +0400

    removing creds file

--- creds81.ps1
+++
@@ -1,6 +0,0 @@
-$user = "AutoUser"
-#TODO: store pw encripted
-$pass = "SomePass2014" | convertto-securestring -asplaintext -force
-$cred = new-object -typename System.Management.Automation.PSCredential -argumentlist $user, $pass
-$arglist = "C:\mytemp\medic-bootstrap\win81.ps1"
-start-process powershell $arglist -Credential $cred
